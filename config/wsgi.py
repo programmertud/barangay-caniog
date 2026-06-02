7fb5c3614c7a33028f0583eea19a373e2849762d
Rianne Celone Tudtud <rtudtud@ssct.edu.ph>
2026-06-02 20:26:51 +0800

    Debug: expose WSGI startup error on Vercel

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import traceback
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    error_msg = traceback.format_exc()
+    print("WSGI STARTUP ERROR:\n", error_msg)
+
+    def application(environ, start_response):
+        status = '500 Internal Server Error'
+        headers = [('Content-Type', 'text/plain; charset=utf-8')]
+        start_response(status, headers)
+        return [b"STARTUP ERROR:\n\n" + error_msg.encode()]
